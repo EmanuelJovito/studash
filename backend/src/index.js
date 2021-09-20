5010752e2f086c98eef4f43d081bdd5cf8aaea2d
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-09-20 14:01:17 -0300

    feat: Creating navigation routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const PORT = 3000
 const HOST = '0.0.0.0'
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Hello World TESTE')
-})
+app.use(routes)
 
 app.listen(PORT, HOST, () => console.log('Server is running'))
