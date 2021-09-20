fed8b451ca61ff1f15e31b1d7911dac70398fba7
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-09-20 09:32:49 -0300

    feat: Config database with knex

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World teste')
+  res.send('Hello World TESTE')
 })
 
-app.listen(PORT, HOST)
+app.listen(PORT, HOST, () => console.log('Server is running'))
