839c65243f318dc58e5c48bc224c4d773146c251
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-09-21 17:54:04 -0300

    feat: Adding new fields to the course table

--- backend/src/database/migrations/20210920123730_create_table_courses.js
+++ backend/src/database/migrations/20210920123730_create_table_courses.js
@@ -3,7 +3,6 @@ exports.up = knex =>
     knex.schema.createTable('courses', table => {
     table.increments('id')
     table.text('course').unique().notNullable()
-    table.text('student_code').unique().notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.text('workload').notNullable()
   })
