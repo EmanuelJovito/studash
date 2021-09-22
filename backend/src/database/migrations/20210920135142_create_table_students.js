0709efdd6a0c66bcdd00e77dd79bbecd399248dd
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-09-22 16:18:34 -0300

    feat: Adding single fields

--- backend/src/database/migrations/20210920135142_create_table_students.js
+++ backend/src/database/migrations/20210920135142_create_table_students.js
@@ -3,10 +3,10 @@ exports.up = knex =>
     knex.schema.createTable('students', table => {
     table.increments('id')
     table.text('student_name').notNullable()
-    table.text('student_CPF').notNullable()
+    table.text('student_CPF').unique().notNullable()
     table.text('student_address').notNullable()
     table.text('student_CEP').notNullable()
-    table.text('student_email').notNullable()
+    table.text('student_email').unique().notNullable()
     table.text('student_number').notNullable()
     table.integer('course_id')
         .references('courses.id')
--- backend/src/database/seeds/001_users.js
+++ backend/src/database/seeds/001_users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
       return knex('students').insert([
         {id: 1, student_name: 'Aluno 1', student_CPF: '321434234134', student_address: 'Av. 1 casa 1', student_CEP: '65050-120',
         student_email: 'aluno1@gmail.com', student_number: '12134', course_id: 1},
-        {id: 2, student_name: 'Aluno 2', student_CPF: '32143423413424', student_address: 'Av. 2 casa 2', student_CEP: '65050-121',
+        {id: 2, student_name: 'Aluno 2', student_CPF: '3214342341342', student_address: 'Av. 2 casa 2', student_CEP: '65050-121',
         student_email: 'aluno2@gmail.com', student_number: '12234', course_id: 1},
         {id: 3, student_name: 'Aluno 3', student_CPF: '32143423413426', student_address: 'Av. 3 casa 3', student_CEP: '65050-122',
         student_email: 'aluno3@gmail.com', student_number: '12334', course_id: 2},
